7213100a764774bf864b7533a50b017b44e3b0ec
RodrigoLimaSolis <rodrigo.lima@solistecnologia.com.br>
2023-11-29 16:44:44 -0300

    Att params nav2

--- src/sobot_drive/description/launch/rsp.launch.py
+++ src/sobot_drive/description/launch/rsp.launch.py
@@ -16,8 +16,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('sobot'))
-    xacro_file = os.path.join(pkg_path,'description/urdf','robot.urdf.xacro')
+    pkg_path = os.path.join(get_package_share_directory('sobot_drive'))
+    xacro_file = os.path.join(pkg_path,'urdf','diffbot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
