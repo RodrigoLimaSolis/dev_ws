4afd45849cb32bb0ea77477e3efe4c0ce88cccb6
RodrigoLimaSolis <rodrigo.lima@solistecnologia.com.br>
2023-12-14 17:30:07 -0300

    Mudando os scripts

--- dev_ws/script/desktop.sh
+++
@@ -1,4 +0,0 @@
-gnome-terminal --tab --command="rviz2 -d rviz2 -d ~/dev_ws/src/sobot_drive/bringup/config/default.rviz"
-gnome-terminal --tab --command="ros2 launch sobot_drive online_async_launch.py"
-gnome-terminal --tab --command="ros2 run twist_mux twist_mux --ros-args --params-file /home/solis/dev_ws/src/sobot_gazebo/config/twist_mux.yaml -r cmd_vel_out:=diffbot_base_controller/cmd_vel_unstamped"
-gnome-terminal --tab --command="ros2 launch sobot_drive navigation_launch.py"
