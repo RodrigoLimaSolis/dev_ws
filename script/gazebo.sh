4e744da63852521a57bc7f912b573a2f51d77af5
RodrigoLimaSolis <rodrigo.lima@solistecnologia.com.br>
2023-12-15 15:46:26 -0300

    Rodando Gazebo

--- script/gazebo.sh
+++ script/gazebo.sh
@@ -1,9 +1,8 @@
 gnome-terminal --tab --command="ros2 launch sobot_gazebo launch_sim.launch.py world:=./src/sobot_gazebo/worlds/world1.world use_sim_time:=true"
-sleep 10
-gnome-terminal --tab --command="sudo cd /home/solis/dev_ws && ros2 run twist_mux twist_mux --ros-args --params-file /home/solis/dev_ws/src/sobot_gazebo/config/twist_mux.yaml -r cmd_vel_out:=/diff_cont//cmd_vel_unstamped"
-sleep 2
+sleep 15
 gnome-terminal --tab --command="ros2 launch sobot_gazebo online_async_launch.py use_sim_time:=true"
 sleep 2
-gnome-terminal --tab --command="ros2 launch nav2_bringup navigation_launch.py use_sim_time:=true"
+gnome-terminal --tab --command="ros2 launch sobot_gazebo navigation_launch.py use_sim_time:=true"
 sleep 2
 gnome-terminal --tab --command="rviz2 use_sim_time:=true"
+gnome-terminal --tab --command="ros2 run teleop_twist_keyboard_sobot teleop_twist_keyboard_sobot --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstamped"
